64dabe379b3869d1dfd638e1abf8c0b0017989fc
Hezka-j <145163687+Hezka-j@users.noreply.github.com>
2024-06-14 11:43:12 +0300

    Add files via upload

---
+++ Console Smile/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Smile
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            int x = 0;
+            int y = 0;
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                Console.CursorLeft = x;
+                Console.CursorTop = y;
+                Console.Write(" ");
+
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+
+                if (key.Key == ConsoleKey.LeftArrow)
+                {
+                    x--;
+                }
+
+                else if (key.Key == ConsoleKey.RightArrow)
+                {
+                    x++;
+
+                }
+
+                else if (key.Key == ConsoleKey.UpArrow)
+                {
+                    y--;
+                }
+
+                else
+                {
+                    y++;
+                }
+
+                if (x == Console.WindowWidth)
+                {
+                    x--;
+                }
+
+                else if (x < Console.WindowLeft)
+                {
+                    x++;
+                }
+
+                else if (y > Console.WindowHeight)
+                {
+                    y--;
+                }
+
+                else if (y < Console.WindowTop)
+                {
+                    y++;
+                }
+
+                Console.CursorLeft = x;
+                Console.CursorTop = y;
+                Console.Write((char)1);
+            }
+        }
+    }
+}
